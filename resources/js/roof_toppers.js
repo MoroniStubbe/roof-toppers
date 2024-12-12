0c27534f5a5facfd7383c998248fe2b138c4c9b4
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-12-12 12:19:22 +0100

    feat add game scripts

---
+++ resources/js/roof_toppers.js
@@ -0,0 +1,11 @@
+
+// Game configuration
+const config = {
+    type: Phaser.AUTO,
+    width: 960,
+    height: 540,
+    scene: RoofToppers,
+};
+
+// Create the game instance
+const game = new Phaser.Game(config);
--- resources/views/roof_toppers.blade.php
+++ resources/views/roof_toppers.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Roof Toppers</title>
     <script src="../node_modules/phaser/dist/phaser.js"></script>
+    <script src="../resources/js/classes/RoofToppers.js"></script>
+    <script src="../resources/js/roof_toppers.js"></script>
 </head>
 
 <body>
-    <script src="../resources/js/classes/RoofToppers.js"></script>
-    <script src="../resources/js/roof_toppers.js"></script>
 </body>
 
 </html>
