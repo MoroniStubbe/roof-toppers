6289ea65e3ba09dddb21b0412a13531d6cacf169
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-12-12 12:19:41 +0100

    feat add game styles

--- resources/views/roof_toppers.blade.php
+++ resources/views/roof_toppers.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Roof Toppers</title>
+    <link rel="stylesheet" href="{{asset('/css/global.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/roof_toppers.css')}}">
     <script src="../node_modules/phaser/dist/phaser.js"></script>
     <script src="../resources/js/classes/RoofToppers.js"></script>
     <script src="../resources/js/roof_toppers.js"></script>
