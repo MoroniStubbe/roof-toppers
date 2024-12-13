23f7c56115425d077eaa6b9d41292324135180a7
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-12-13 14:21:26 +0100

    feat add create_platforms() to Game

--- resources/js/classes/RoofToppers.js
+++ resources/js/classes/RoofToppers.js
@@ -4,6 +4,13 @@ class RoofToppers extends Phaser.Scene {
     }
 
     preload() {
+
+    create_platforms() {
+        this.platforms = this.physics.add.staticGroup();
+
+        PLATFORMS_CONFIG.forEach(platform_data => {
+            new Platform(this, platform_data.x, platform_data.y, platform_data.width, platform_data.height, platform_data.key);
+        });
     }
 
     create() {
