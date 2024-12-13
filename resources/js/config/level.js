73b7ac9507a3f405f6ea66ed501dd555071fb1bd
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-12-13 14:20:10 +0100

    feat add example level

---
+++ resources/js/config/level.js
@@ -0,0 +1,5 @@
+const PLATFORMS_CONFIG = [
+    { x: 100, y: 100, width: 200, height: 50, key: 'platform_image' },
+    { x: 200, y: 200, width: 200, height: 50, key: 'platform_image' },
+    { x: 300, y: 300, width: 200, height: 50, key: 'platform_image' },
+];
