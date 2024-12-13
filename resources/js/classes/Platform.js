6a62bae2ec9579e18de904ff30b7431dcee2c312
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-12-13 14:29:29 +0100

    fix tile scale

--- resources/js/classes/Platform.js
+++ resources/js/classes/Platform.js
@@ -1,7 +1,7 @@
 class Platform {
     constructor(scene, x, y, width, height, key) {
         const PLATFORM = scene.add.tileSprite(x, y, width, height, key).setOrigin(0, 0);
-        const TILE_SCALE = 0.1;
+        const TILE_SCALE = 0.07;
         PLATFORM.setTileScale(TILE_SCALE, TILE_SCALE);
         scene.physics.add.existing(PLATFORM, true);
     }
