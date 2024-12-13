f5da6d86b08d267992c9a5b67fa0a8a56cdb38ae
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-12-13 14:19:35 +0100

    chore add imports

--- resources/views/roof_toppers.blade.php
+++ resources/views/roof_toppers.blade.php
@@ -5,9 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Roof Toppers</title>
-    <link rel="stylesheet" href="{{asset('/css/global.css')}}">
-    <link rel="stylesheet" href="{{asset('/css/roof_toppers.css')}}">
+    <link rel="stylesheet" href="{{ asset('/css/global.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/roof_toppers.css') }}">
     <script src="../node_modules/phaser/dist/phaser.js"></script>
+    <script src="../resources/js/config/level.js"></script>
+    <script src="../resources/js/classes/Platform.js"></script>
     <script src="../resources/js/classes/RoofToppers.js"></script>
     <script src="../resources/js/roof_toppers.js"></script>
 </head>
