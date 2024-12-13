c57346002a680c262b613a199c466b259e20009c
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-12-13 14:08:30 +0100

    fix add physics to config

--- resources/js/roof_toppers.js
+++ resources/js/roof_toppers.js
@@ -1,10 +1,17 @@
 // Game configuration
-const config = {
+const CONFIG = {
     type: Phaser.AUTO,
     width: 960,
     height: 540,
     scene: RoofToppers,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
 };
 
 // Create the game instance
-const game = new Phaser.Game(config);
+const GAME = new Phaser.Game(CONFIG);
