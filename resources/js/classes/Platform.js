03f703249d12adf3da3531442e29766c869dc4d7
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-12-16 14:30:38 +0100

    fix make platform use fixed size and image
    fix make platform use fixed size and image
    
    fix make platform use fixed size and image
    
    fix make platform use fixed size and image

--- resources/js/classes/Platform.js
+++ resources/js/classes/Platform.js
@@ -1,6 +1,6 @@
 class Platform extends Phaser.GameObjects.TileSprite {
-    constructor(scene, x, y, width, height, key) {
-        super(scene, x, y, width, height, key);
+    constructor(scene, x, y) {
+        super(scene, x, y, 200, 50, 'platform_image');
         scene.add.existing(this);
         this.setOrigin(0, 0);
         const TILE_SCALE = 0.07;
--- resources/js/classes/RoofToppers.js
+++ resources/js/classes/RoofToppers.js
@@ -13,7 +13,7 @@ class RoofToppers extends Phaser.Scene {
         this.platforms = this.physics.add.staticGroup();
 
         PLATFORMS_CONFIG.forEach(platform_data => {
-            new Platform(this, platform_data.x, platform_data.y, platform_data.width, platform_data.height, platform_data.key);
+            new Platform(this, platform_data.x, platform_data.y);
         });
     }
 
