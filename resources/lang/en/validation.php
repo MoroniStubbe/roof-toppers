ba03007242481916d6d1e3578eddc1817f147d71
Aidan De Groot <119511296+AidanjdeGroot@users.noreply.github.com>
2025-01-13 13:39:03 +0100

    Create validation.php

---
+++ resources/lang/en/validation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'required' => 'The :attribute field is required.',
+    'unique' => 'This :attribute has already been taken.',
+    'name.required' => 'A gamertag is required.',
+    'attributes' => [
+        'name' => 'gamertag',
+    ],
+];
