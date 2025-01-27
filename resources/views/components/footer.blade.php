c4ae18af9b5bfb0f4db4866f0ca308d90d15d034
KeviNVHAL <kevinvhal2005@gmail.com>
2025-01-27 14:11:15 +0100

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer style="text-align:center; position: absolute; bottom: 0; width: 99%;">
+<footer style="text-align:center; color: white; position: absolute; bottom: 0; width: 99%;">
     <div>
         <p>© 2025 Rooftoppers made by Moroni, Kevin, Aidan and Shivam</p>
     </div>
